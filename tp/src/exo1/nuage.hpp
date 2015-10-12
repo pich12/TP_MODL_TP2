56d04d8357011c807eda2cc91468961b0872c858
thomas PICHON <pichon@etud.rcisima.isima.fr>
2015-10-12 14:46:23 +0200

    V0.1 bis (avec base)

---
+++ tp/src/exo1/nuage.hpp
@@ -0,0 +1,40 @@
+// Gardien //---------------------------------------------------------------------------------------
+#ifndef _NUAGE_HPP_
+#define _NUAGE_HPP_
+
+// Entetes //---------------------------------------------------------------------------------------
+#include <vector>
+#include <cartesien.hpp>
+#include <polaire.hpp>
+
+// Declarations anticipees //-----------------------------------------------------------------------
+class Nuage;
+
+// Declarations fonctions //------------------------------------------------------------------------
+Cartesien barycentre(const Nuage &);
+
+// Classe  N u a g e //-----------------------------------------------------------------------------
+class Nuage {
+ //---------------------------------------------------------------------------------------------Type
+ public: typedef std::vector<Point *>::const_iterator iterator;
+ //----------------------------------------------------------------------------------------Attributs
+ protected: std::vector<Point *> points_;
+ //---------------------------------------------------------------------------------------Accesseurs
+ public: std::vector<Point *>::const_iterator begin() const { return points_.begin(); }
+ public: std::vector<Point *>::const_iterator end() const { return points_.end(); }
+ //-------------------------------------------------------------------------------Methodes publiques
+ public: void ajouter(Point * point) { points_.push_back(point); }
+};
+
+// Classe  B a r y c e n t r e C a r t e s i e n //-------------------------------------------------
+class BarycentreCartesien {
+ public: Cartesien operator () (const Nuage & nuage) { return barycentre(nuage); }
+};
+
+// Classe  B a r y c e n t r e P o l a i r e //-----------------------------------------------------
+class BarycentrePolaire {
+ public: Polaire operator () (const Nuage & nuage) { return Polaire(barycentre(nuage)); }
+};
+
+// Fin //-------------------------------------------------------------------------------------------
+#endif
---
+++ tp/src/exo1/nuageGen.hpp
@@ -0,0 +1,42 @@
+// Gardien //---------------------------------------------------------------------------------------
+#ifndef _NUAGEGEN_HPP_
+#define _NUAGEGEN_HPP_
+
+// Entetes //---------------------------------------------------------------------------------------
+#include <vector>
+#include <cartesien.hpp>
+#include <polaire.hpp>
+
+template <typename T>
+
+// Declarations anticipees //-----------------------------------------------------------------------
+class NuageGen;
+
+// Declarations fonctions //------------------------------------------------------------------------
+Cartesien barycentre(const NuageGen &);
+
+// Classe  N u a g e //-----------------------------------------------------------------------------
+class NuageGen {
+ //---------------------------------------------------------------------------------------------Type
+ public: typedef std::vector<T *>::const_iterator iterator;
+ //----------------------------------------------------------------------------------------Attributs
+ protected: std::vector<T *> points_;
+ //---------------------------------------------------------------------------------------Accesseurs
+ public: std::vector<T *>::const_iterator begin() const { return points_.begin(); }
+ public: std::vector<T *>::const_iterator end() const { return points_.end(); }
+ //-------------------------------------------------------------------------------Methodes publiques
+ public: void ajouter(T * point) { points_.push_back(point); }
+};
+
+// Classe  B a r y c e n t r e C a r t e s i e n //-------------------------------------------------
+class BarycentreCartesien {
+ public: Cartesien operator () (const NuageGen & nuage) { return barycentre(nuage); }
+};
+
+// Classe  B a r y c e n t r e P o l a i r e //-----------------------------------------------------
+class BarycentrePolaire {
+ public: Polaire operator () (const NuageGen & nuage) { return Polaire(barycentre(nuage)); }
+};
+
+// Fin //-------------------------------------------------------------------------------------------
+#endif
