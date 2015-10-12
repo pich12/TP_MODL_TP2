b6d66721c72d8df196765da6fc12ba426a30dd47
thomas PICHON <pichon@etud.rcisima.isima.fr>
2015-10-12 14:03:47 +0200

    Version 0 Correction TP1

---
+++ tp/src/main.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+
+//#include "point.hpp"
+#include "cartesien.hpp"
+#include "polaire.hpp"
+
+int main() {
+	
+/*	Point c = Cartesien:Cartesien();
+	Polaire p = Polaire:Polaire();
+	
+	//Vector<Point> vect = Vector<Point>();
+*/
+	
+
+	
+ return EXIT_SUCCESS;
+}
